065ccfd689c3c9e92e9003e6a641016b8af7cce6
hyun031916 <hyun031916@gmail.com>
2021-04-09 00:23:34 +0900

    간단한 프로젝트를 통해 IOC 컨테이너등의 Spring의 구조 학습하기

---
+++ BasicSpring/src/main/java/kr/hs/study/beans/lgTv.java
@@ -0,0 +1,25 @@
+package kr.hs.study.beans;
+
+public class lgTv implements Tv {
+
+	public void volumeUp() {
+		// TODO Auto-generated method stub
+		System.out.println("lgTv volumeUp()");
+	}
+
+	public void volumeDown() {
+		// TODO Auto-generated method stub
+		System.out.println("lgTv volumeDown()");
+	}
+
+	public void powerOn() {
+		// TODO Auto-generated method stub
+		System.out.println("lgTv powerOn()");
+	}
+
+	public void powerOff() {
+		// TODO Auto-generated method stub
+		System.out.println("lgTv powerOff()");
+	}
+
+}
---
+++ BasicSpring/src/main/java/kr/hs/study/beans/samsungTv.java
@@ -0,0 +1,25 @@
+package kr.hs.study.beans;
+
+public class samsungTv implements Tv {
+
+	public void volumeUp() {
+		// TODO Auto-generated method stub
+		System.out.println("samsungTv volumeUp()");
+	}
+
+	public void volumeDown() {
+		// TODO Auto-generated method stub
+		System.out.println("samsungTv volumeDown()");
+	}
+
+	public void powerOn() {
+		// TODO Auto-generated method stub
+		System.out.println("samsungTv powerOn()");
+	}
+
+	public void powerOff() {
+		// TODO Auto-generated method stub
+		System.out.println("samsungTv powerOff()");
+	}
+
+}
