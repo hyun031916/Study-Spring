1f3013d9e3745f3a1db3d755ee663babc2b5f81e
hyun031916 <hyun031916@gmail.com>
2021-04-13 10:15:06 +0900

    aop - advisor(before, after, around(리턴타입 유무), after-returning, after-throwing 사용해보기

---
+++ AOP2/src/main/java/kr/hs/study/main/MainClass.java
@@ -0,0 +1,28 @@
+package kr.hs.study.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import kr.hs.study.beans.TestBean1;
+import kr.hs.study.beans.TestBean2;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("kr/hs/study/config/config.xml");
+		
+		TestBean1 t1 = ctx.getBean("t1", TestBean1.class);
+		TestBean2 t2 = ctx.getBean("t2", TestBean2.class);
+		t1.method1();
+		t1.method2();
+		t1.method3();
+//		t1.func1();
+		t1.func2();
+		t1.func3();
+		System.out.println(t1.func4());
+		t2.method4();
+		
+		System.out.println("=======================================");
+	}
+
+}
